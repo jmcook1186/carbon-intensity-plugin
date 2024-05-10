56a75e98813f904b43c1f7f430500426d442ac63
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-05-10 16:56:35 +0100

    feat(lib): add errors

---
+++ src/util/errors.ts
@@ -0,0 +1,25 @@
+const CUSTOM_ERRORS = [
+  'AuthorizationError',
+  'APIRequestError',
+  'InputValidationError',
+  'UnsupportedValueError',
+  'ConfigValidationError',
+] as const;
+
+type CustomErrors = {
+  [K in (typeof CUSTOM_ERRORS)[number]]: ErrorConstructor;
+};
+
+export const ERRORS = CUSTOM_ERRORS.reduce((acc, className) => {
+  acc = {
+    ...acc,
+    [className]: class extends Error {
+      constructor(message: string) {
+        super(message);
+        this.name = this.constructor.name;
+      }
+    },
+  };
+
+  return acc;
+}, {} as CustomErrors);
