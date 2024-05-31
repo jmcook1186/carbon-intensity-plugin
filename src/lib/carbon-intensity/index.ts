237b923b71fb488fe423c6d58a4bf6833fa8cb7e
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-05-31 11:24:09 +0100

    only make api requests if 30 mins have elapsed since last api call

--- src/lib/carbon-intensity/index.ts
+++ src/lib/carbon-intensity/index.ts
@@ -1,5 +1,5 @@
-import {PluginInterface, PluginParams} from '../types/interface';
-import {CarbonIntensityAPI} from './api';
+import { PluginInterface, PluginParams } from '../types/interface';
+import { CarbonIntensityAPI } from './api';
 
 export const CarbonIntensityPlugin = (): PluginInterface => {
   const metadata = {
@@ -11,10 +11,18 @@ export const CarbonIntensityPlugin = (): PluginInterface => {
    */
   const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
     const result = [];
+    const startDate = new Date(inputs[0].timestamp);
+    let intensity = await CarbonIntensityAPI().fetchOutputData(inputs[0].timestamp);
+
     for await (const input of inputs) {
-      const date = input.timestamp;
-      const intensity = await CarbonIntensityAPI().fetchOutputData(date);
-      result.push({...input, 'grid/carbon-intensity': intensity});
+
+      const snapshot = new Date(input.timestamp);
+      const diff = (snapshot.getTime() - startDate.getTime()) / 1000 / 60 / 60;
+      if (diff >= 0.5) {
+        intensity = await CarbonIntensityAPI().fetchOutputData(input.timestamp);
+        console.log(`carbon-intensity plugin: ${input.timestamp} 30 mins have elapsed, making new api request`)
+      }
+      result.push({ ...input, 'grid/carbon-intensity': intensity });
     }
     return result;
   };
